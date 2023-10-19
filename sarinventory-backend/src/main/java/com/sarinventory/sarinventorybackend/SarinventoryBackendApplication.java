6e2ad4e06ac5035bb44b0618a67d29cc156a8dd1
InazumAddict23 <105416962+InazumAddict23@users.noreply.github.com>
2023-10-19 22:21:40 +0800

    added backend and frontend folder

---
+++ sarinventory-backend/src/main/java/com/sarinventory/sarinventorybackend/SarinventoryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sarinventory.sarinventorybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarinventoryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarinventoryBackendApplication.class, args);
+	}
+
+}
